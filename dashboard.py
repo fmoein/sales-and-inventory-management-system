ea8899cc0d470bdc11c967c27d50038a352238d6
maryamgoodarzi1425 <maryamgoodarzi1425@gmail.com>
2025-11-03 10:01:10 +0330

    Stats card and addressing fix

--- dashboard.py
+++ dashboard.py
@@ -3,11 +3,11 @@ from tkinter import *
 window=Tk()
 
 window.title('Dashboard')
-window.geometry('1270x668+0+0')
+window.state('zoomed')
 window.resizable(0,0)
 window.config(bg='#fef9fb')
 
-bg_image=PhotoImage(file='inventory.png')
+bg_image=PhotoImage(file='images/inventory.png')
 titleLable=Label(window,image=bg_image,compound=LEFT,text='                               سیستم فروش و انبار داری ',font=('Yekan',30,'bold'), bg='#813ffe',fg='#07070a',anchor='w',padx=20)
 titleLable.place(x=0,y=0,relwidth=1)
 
@@ -18,45 +18,112 @@ SubtitleLabel=Label(window,text='ادمین خوش آمدید\t\t تاریخ: 01
 SubtitleLabel.place(x=0,y=70,relwidth=1)
 
 leftFrame=Frame(window)
-leftFrame.place(x=0,y=107,width=200,height=560)
+leftFrame.place(x=0,y=120,width=200,height=560)
 
-LogoImage=PhotoImage(file='checklist-1.png')
+LogoImage=PhotoImage(file='images/checklist-1.png')
 imageLable=Label(leftFrame,image=LogoImage)
 imageLable.pack()
 
 
-menuLabel=Label(leftFrame,text='منو',font=('Yekan',13),bg='#00198f')
+menuLabel=Label(leftFrame,text='منو',font=('Yekan',14,'bold'),bg='#00198f')
 menuLabel.pack(fill=X)
 
 
-employee_icon=PhotoImage(file='employee.png')
-employee_button=Button(leftFrame,image=employee_icon,compound=LEFT,text='         کارمندان',font=('Yekan',15,'bold'))
+employee_icon=PhotoImage(file='images/employee.png')
+employee_button=Button(leftFrame,image=employee_icon,compound=LEFT,text='          کارمندان',font=('Yekan',15,'bold'))
 employee_button.pack(fill=X)
 
 
-supplier_icon=PhotoImage(file='supplier.png')
-supplier_button=Button(leftFrame,image=supplier_icon,compound=LEFT,text='  تامین کنندگان',font=('Yekan',15,'bold'))
+supplier_icon=PhotoImage(file='images/supplier.png')
+supplier_button=Button(leftFrame,image=supplier_icon,compound=LEFT,text='   تامین کنندگان',font=('Yekan',15,'bold'))
 supplier_button.pack(fill=X)
 
 
-category_icon=PhotoImage(file='category.png')
+category_icon=PhotoImage(file='images/category.png')
 category_button=Button(leftFrame,image=category_icon,compound=LEFT,text='       دسته بندی ',font=('Yekan',15,'bold'))
 category_button.pack(fill=X)
 
-products_icon=PhotoImage(file='products.png')
+products_icon=PhotoImage(file='images/products.png')
 products_button=Button(leftFrame,image=products_icon,compound=LEFT,text='         محصولات ',font=('Yekan',15,'bold'))
 products_button.pack(fill=X)
 
-sale_icon=PhotoImage(file='sale.png')
-sale_button=Button(leftFrame,image=sale_icon,compound=LEFT,text='          تخفیفات',font=('Yekan',15,'bold'))
+sale_icon=PhotoImage(file='images/sale.png')
+sale_button=Button(leftFrame,image=sale_icon,compound=LEFT,text='             فروش',font=('Yekan',15,'bold'))
 sale_button.pack(fill=X)
 
-exit_icon=PhotoImage(file='exit.png')
+exit_icon=PhotoImage(file='images/exit.png')
 exit_button=Button(leftFrame,image=exit_icon,compound=LEFT,text='             خروج',font=('Yekan',15,'bold'))
 exit_button.pack(fill=X)
 
 
+emp_frame=Frame(window,bg='#00198f',bd=4,relief=RIDGE)
+emp_frame.place(x=400,y=125,height=170,width=280)
+totl_emp_icon=PhotoImage(file='images/total_employee.png')
+totl_emp_icon_label=Label(emp_frame,image=totl_emp_icon,bg='#00198f')
+totl_emp_icon_label.pack(pady=8)
+
+totl_emp_label=Label(emp_frame,text='تعداد کارمندان',bg='#00198f',fg='#fef9fb',font=('Yekan',15,'bold'))
+totl_emp_label.pack()
+
+totl_emp_count=Label(emp_frame,text='0',bg='#00198f',fg='#fef9fb',font=('Yekan',25,'bold'))
+totl_emp_count.pack()
 
 
+
+
+sup_frame=Frame(window,bg='#00198f',bd=4,relief=RIDGE)
+sup_frame.place(x=800,y=125,height=170,width=280)
+totl_sup_icon=PhotoImage(file='images/total_sup.png')
+totl_sup_icon_label=Label(sup_frame,image=totl_sup_icon,bg='#00198f')
+totl_sup_icon_label.pack(pady=8)
+
+totl_sup_label=Label(sup_frame,text=' تعداد تامین کنندگان  ',bg='#00198f',fg='#fef9fb',font=('Yekan',15,'bold'))
+totl_sup_label.pack()
+
+totl_sup_count=Label(sup_frame,text='0',bg='#00198f',fg='#fef9fb',font=('Yekan',25,'bold'))
+totl_sup_count.pack()
+
+
+
+
+category_frame=Frame(window,bg='#00198f',bd=4,relief=RIDGE)
+category_frame.place(x=400,y=310,height=170,width=280)
+totl_category_icon=PhotoImage(file='images/total_category.png')
+totl_category_icon_label=Label(category_frame,image=totl_category_icon,bg='#00198f')
+totl_category_icon_label.pack(pady=8)
+
+totl_category_label=Label(category_frame,text=' تعداد دسته بندی ها  ',bg='#00198f',fg='#fef9fb',font=('Yekan',15,'bold'))
+totl_category_label.pack()
+
+totl_category_count=Label(category_frame,text='0',bg='#00198f',fg='#fef9fb',font=('Yekan',25,'bold'))
+totl_category_count.pack()
+
+
+product_frame=Frame(window,bg='#00198f',bd=4,relief=RIDGE)
+product_frame.place(x=800,y=310,height=170,width=280)
+totl_product_icon=PhotoImage(file='images/total_product.png')
+totl_product_icon_label=Label(product_frame,image=totl_product_icon,bg='#00198f')
+totl_product_icon_label.pack(pady=8)
+
+totl_product_label=Label(product_frame,text='    تعداد محصولات     ',bg='#00198f',fg='#fef9fb',font=('Yekan',15,'bold'))
+totl_product_label.pack()
+
+totl_product_count=Label(product_frame,text='0',bg='#00198f',fg='#fef9fb',font=('Yekan',25,'bold'))
+totl_product_count.pack()
+
+
+
+
+sale_frame=Frame(window,bg='#00198f',bd=4,relief=RIDGE)
+sale_frame.place(x=600,y=495,height=170,width=280)
+totl_sale_icon=PhotoImage(file='images/total_sale.png')
+totl_sale_icon_label=Label(sale_frame,image=totl_sale_icon,bg='#00198f')
+totl_sale_icon_label.pack(pady=8)
+
+totl_sale_label=Label(sale_frame,text=' تعداد فروش ',bg='#00198f',fg='#fef9fb',font=('Yekan',15,'bold'))
+totl_sale_label.pack()
+
+totl_sale_count=Label(sale_frame,text='0',bg='#00198f',fg='#fef9fb',font=('Yekan',25,'bold'))
+totl_sale_count.pack()
 window.mainloop()
 
