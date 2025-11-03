ca5b1240d069907f958f85ee3a26fad7364df7b6
maryamgoodarzi1425 <maryamgoodarzi1425@gmail.com>
2025-11-03 09:14:39 +0330

    Create dashboard with bg color

---
+++ dashboard.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window=Tk()
+
+window.title('Dashboard')
+window.geometry('1270x668+0+0')
+window.resizable(0,0)
+window.config(bg='white')
+
+titleLable=Label(window)
+window.mainloop()
+
