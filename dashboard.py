71ddf32efda512e1f5a4a5bcb65a171fd23eeaaf
maryamgoodarzi1425 <maryamgoodarzi1425@gmail.com>
2025-11-03 09:40:29 +0330

    Add top icon

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,12 @@ window.geometry('1270x668+0+0')
 window.resizable(0,0)
 window.config(bg='white')
 
-titleLable=Label(window,text='سیستم فروش و انبار داری',font=('Yekan',30,'bold'), bg='#813ffe',fg='#07070a')
-titleLable.place(x=0,y=0)
+bg_image=PhotoImage(file='inventory.png')
+titleLable=Label(window,image=bg_image,compound=LEFT,text='                               سیستم فروش و انبار داری ',font=('Yekan',30,'bold'), bg='#813ffe',fg='#07070a',anchor='w',padx=20)
+titleLable.place(x=0,y=0,relwidth=1)
+
+logoButten=Button(window,text='خروج',font=('Yekan',16,'bold'),fg='#07070a')
+logoButten.place(x=1100,y=10)
+
 window.mainloop()
 
